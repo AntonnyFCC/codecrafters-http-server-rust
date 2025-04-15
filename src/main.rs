caa93a628d01e97e5df4a3d63721d7b7b5fcb760
AntonnyFCC <antonnycc01@gmail.com>
2025-04-15 15:37:45 -0500

    add param handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{Read, Write};
+use regex::Regex;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -20,11 +21,25 @@ fn main() {
                         let request_line = request.lines().next().unwrap_or("");
 
                         let response = match request_line {
-                            "GET / HTTP/1.1" | "GET /index.html HTTP/1.1" => {
-                                "HTTP/1.1 200 OK\r\n\r\n"
+                            "GET / HTTP/1.1" => {
+                                "HTTP/1.1 200 OK\r\n\r\n".to_string()
+                            }
+                            request_line if request_line.starts_with("GET /echo") => {
+                                let re = Regex::new(r"GET /echo/(.*) HTTP/1.1").unwrap();
+                                if let Some(caps) = re.captures(request_line) {
+                                    if let Some(param) = caps.get(1) {
+                                        let param = param.as_str();
+                                        let param_length = param.len();
+                                        format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", param_length, param)
+                                    } else {
+                                        "HTTP/1.1 400 Bad Request\r\n\r\n".to_string()
+                                    }
+                                } else {
+                                    "HTTP/1.1 400 Bad Request\r\n\r\n".to_string()
+                                }
                             }
                             _ => {
-                                "HTTP/1.1 404 Not Found\r\n\r\n"
+                                "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
                             }
                             
                         };
